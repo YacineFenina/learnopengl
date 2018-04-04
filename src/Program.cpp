23d01cca2197768f6deff70b6ba9f72ecc64a375
Yacine Fenina <yacine.fenina@gmail.com>
2018-04-04 23:04:27 +0200

    Add first version of OpenGL program to draw a triangle
    We use this tutorial to write the program : https://learnopengl.com/
    This one doesn't work. We will try with GLAD as in the tutorial

---
+++ src/Program.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+
+float vertices[] = 
+{
+    -0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+};
+
+string ReadShader(char const *filePath)
+{
+    string res("");
+    ifstream shaderStream(filePath, ios::in);
+    if (shaderStream.is_open())
+    {
+        stringstream sstr;
+        sstr << shaderStream.rdbuf();
+        res = sstr.str();
+        shaderStream.close();
+    }
+    else
+    {
+        cout << "Impossible to find your shader : " << filePath << "\n";
+        getchar();
+    }
+    return res;
+}
+
+
+int main()
+{
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Triangle", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = true; // Necessary for based profile
+	if (glewInit() != GLEW_OK)
+	{
+		cout << "Failed to initialize GLEW! "
+         << "\n";
+		glfwTerminate();
+		return false;
+	}
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+
+    glViewport(0, 0, 800, 600);
+
+
+
+
+
+    unsigned int VBO;
+    cout << "??? \n";
+    glGenBuffers(1, &VBO);
+
+
+
+
+
+
+    unsigned int vertexShaderID(glCreateShader(GL_VERTEX_SHADER));
+    string vertexShader(ReadShader("shaders/vertexShader.glsl"));
+    char const *vertexSource = vertexShader.c_str();    
+    glShaderSource(vertexShaderID, 1, &vertexSource, NULL);
+    glCompileShader(vertexShaderID);
+
+    unsigned int fragmentShaderID(glCreateShader(GL_FRAGMENT_SHADER));
+    string fragmentShader(ReadShader("shaders/fragmentShader.glsl"));
+    char const *fragmentSource = fragmentShader.c_str();
+    
+    glShaderSource(fragmentShaderID, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShaderID);
+
+
+
+    unsigned int shaderProgramID(glCreateProgram());
+    glAttachShader(shaderProgramID, vertexShaderID);
+    glAttachShader(shaderProgramID, fragmentShaderID);
+    glLinkProgram(shaderProgramID);
+
+    glDeleteShader(vertexShaderID);
+    glDeleteShader(fragmentShaderID);
+
+
+
+    unsigned int VAO(0);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+    // ============================================
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    //=============================
+
+
+
+    while (!glfwWindowShouldClose(window))
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);        
+
+        glUseProgram(shaderProgramID);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
